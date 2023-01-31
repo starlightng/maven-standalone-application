f11c0738a783ad2cb5ccc82efcc6a8b6e072d0a6
starlightng <100170729+starlightng@users.noreply.github.com>
2023-01-31 20:44:55 +0100

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,12 +9,12 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Engineers, Welcome to Landmark Technologies DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Engineers, Welcome to LearnWithPride DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Landmark Technologies";
+		String m = "LearnWithPride Solutions";
 		return new Date();
 
 	}
